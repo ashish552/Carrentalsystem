042c1909d4c3ea67d819a5c17b4b088d8c779d5f
ashish552 <78848681+ashish552@users.noreply.github.com>
2025-05-26 22:12:01 +1200

    Updating main file and validation

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from database import init_db
 from user import UserManager
+from admin import AdminManager
+from customer import CustomerManager
 
 def menu():
 
-    print("\n ---- user manager -------")
+    print(" \n---- user manager -------")
     print("1. Register")
     print("2. Login")
     print("3. Exit")
 
 def main():
     user_manager = UserManager()
+    customer_manager = CustomerManager()
+    admin_manager = AdminManager()
     init_db()
 
     while True:
@@ -21,7 +25,28 @@ def main():
             user = user_manager.login()
             if user:
                 user_id, _, role = user
-                print(f"✅ Logged in as {role.capitalize()} (User ID: {user_id})")
+                if role == "admin":
+                   while True:
+                    admin_action = input(" \n1. Add car\n2. Update car\n3. Delete car\n4. Manage Bookings\n5. Logout\nselect an option from (1-5): ")
+                    if admin_action == "1":
+                        admin_manager.add_car()
+                    elif admin_action == "2":
+                        admin_manager.update_car()
+                    elif admin_action == "3":
+                        admin_manager.delete_car()
+                    elif admin_action == "4":
+                        admin_manager.manage_bookings()
+                    elif admin_action == "5":
+                        break
+                elif role == "customer":
+                    while True:
+                        cust_action = input("\n1. View cars details\n2. Book car\n3. Logout \nSelect an option from (1-3): ")
+                        if cust_action == '1':
+                            customer_manager.view_available_cars()
+                        elif cust_action == '2':
+                            customer_manager.book_car(user_id)
+                        elif cust_action == '3':
+                            break
         elif choice == "3":
             break
 
